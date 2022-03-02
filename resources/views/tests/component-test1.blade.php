ff0d6e29c7e77a616a5734d2dd7d5d311792a427
先規　勇樹 <yukisaki80@gmail.com>
2022-03-02 23:26:21 +0900

    属性の表示

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,5 @@
   </x-slot>
   コンポーネントテスト１
 
-  <x-tests.card title="タイトル" content="本文"></x-tests.card>
+  <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
 </x-tests.app>
