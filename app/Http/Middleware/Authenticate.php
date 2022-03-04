777172e650cf56ca39d35b5f65aa7d648048a9ea
先規　勇樹 <yukisaki80@gmail.com>
2022-03-05 08:15:50 +0900

    authを修正

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    protected $user_root = 'user.login';
+    protected $owner_root = 'owner.login';
+    protected $admin_root = 'admin.login';
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -15,7 +20,13 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            if(Route::id('owner.*')){
+                return route($this->owner_root);
+            } elseif(Route::id('admin.*')){
+                return route($this->admin_root);
+            } else {
+                return route($this->user_root);
+            }
         }
     }
 }
