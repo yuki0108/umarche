dd8fb0049c14714fa73c42453005449775f5ed88
先規　勇樹 <yukisaki80@gmail.com>
2022-03-07 08:31:41 +0900

    flshメッセージを追加

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,14 @@
+@props(['status' => 'info'])
+
+
+@php
+$bgColor = 'bg-blue-300';
+if(@status === 'info'){ $bgColor = 'bg-blue-300';}
+if(@status === 'error'){ $bgColor = 'bg-red-500';}
+@endphp
+
+@if(session('message'))
+  <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+    {{ session('message') }}
+  </div>
+@endif
