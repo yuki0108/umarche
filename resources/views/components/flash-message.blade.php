7a2ac83034933099a103c1903aeb0622035e13b7
先規　勇樹 <yukisaki80@gmail.com>
2022-03-09 07:55:12 +0900

    論理削除を追加

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -3,8 +3,8 @@
 
 @php
 $bgColor = 'bg-blue-300';
-if(@status === 'info'){ $bgColor = 'bg-blue-300';}
-if(@status === 'error'){ $bgColor = 'bg-red-500';}
+if(session('status') === 'info'){ $bgColor = 'bg-blue-300';}
+if(session('status') === 'alert'){ $bgColor = 'bg-red-500';}
 @endphp
 
 @if(session('message'))
