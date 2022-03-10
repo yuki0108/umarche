656f5e0331be023abe4739d1efcbb297e0a6ba7d
先規　勇樹 <yukisaki80@gmail.com>
2022-03-10 22:22:31 +0900

    論理削除を実装

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -147,4 +147,14 @@ class OwnersController extends Controller
             'status' => 'alert'
         ]);
     }
+
+    public function expiredOwnerIndex() {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners',compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id) {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
+    }
 }
