1327cf7198c009f3faa666300254e172e4fd0c84
先規　勇樹 <yukisaki80@gmail.com>
2022-04-06 23:27:12 +0900

    cascadeを追加

--- database/migrations/2022_03_10_225949_create_shops_table.php
+++ database/migrations/2022_03_10_225949_create_shops_table.php
@@ -15,7 +15,10 @@ class CreateShopsTable extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('owner_id')->constrained();
+            $table->foreignId('owner_id')
+            ->constrained()
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
             $table->string('name');
             $table->text('information');
             $table->string('filename');
